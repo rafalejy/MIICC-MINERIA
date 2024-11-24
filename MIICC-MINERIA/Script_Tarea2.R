028b9aae390a763f2572aa76fa025aa29d55f079
Rafael Juarez <rafalejy@gmail.com>
2024-11-24 15:04:55 -0600

    agregadas tareas

--- MIICC-MINERIA/Script_Tarea2.R
+++ MIICC-MINERIA/Script_Tarea2.R
@@ -9,20 +9,4 @@ data2 <-  data[ , !(names(data) %in% c("PEA", "POCUPA", "PEI"))]
 reglas <- apriori(data2, parameter= list(support=0.2, confidence=0.5) )
 inspect(reglas[1700:1800])
 
-# summary(data )
-# 
-# inspect(reglas[0:100])
-# 
-# reglas_2 <- subset(reglas, lhs %pin% "PCH9_H")
-# 
-# reglas_ordenadas <- sort(reglas_2, by = "confidence", decreasing = TRUE)
-# 
-# # Inspeccionar las primeras 100 reglas con mayor confianza
-# inspect(reglas_ordenadas[100:200])
-# 
-# data2 <- subset(data, DEPARTAMENTO ==1)
-# data2 = data2[,-1]
-# 
-# reglas2 <- apriori(data2, parameter= list(support=0.2, confidence=0.5) )
-# 
-# inspect(reglas2[100:200])
+cluster <- kmeans ( data2, centers = 3)
--- MIICC-MINERIA/Scritpt tarea 4.R
+++ MIICC-MINERIA/Scritpt tarea 4.R
@@ -16,8 +16,13 @@ head (data2, n=10)
 install.packages("ggplot2")
 library(ggplot2)
 
+library(dplyr)
+
+
 data3 <- data2[, c("ANEDUCA", "PCP26_C")]
 data3 <- na.omit(data3)
+
+data3 <- sample_n(data3, 100000)
 
 cluster <- kmeans ( data3, centers = 3)
 
@@ -25,17 +30,17 @@ print(
 ggplot(data3, aes(x = PCP26_C, y = ANEDUCA, color = as.factor(cluster$cluster)))+
   geom_point()+
   geom_point(data = as.data.frame(cluster$centers), aes(x=PCP26_C, y = ANEDUCA), color = "black", size=4, shape=17)+
-  labs(title = "Edad vs Nota final")+
+  labs(title = "Uso de Internet vs Años de Educacion")+
+  scale_x_continuous(trans = 'identity') +  # Escala lineal en el eje X
+  scale_y_continuous(trans = 'identity') +  # Escala lineal en el eje Y
   theme_minimal()
 )
 
-g <- ggplot(data3, aes(x = PCP26_C, y = ANEDUCA, color = cluster_col)) +
-  geom_point() +
-  geom_point(data = as.data.frame(cluster$centers), aes(x = PCP26_C, y = ANEDUCA), color = "black", size = 4, shape = 17) +
-  labs(title = "Edad vs Nota final") +
+g <- ggplot(data3, aes(x = PCP26_C, y = ANEDUCA, color = as.factor(cluster$cluster)))+
+  geom_point()+
+  geom_point(data = as.data.frame(cluster$centers), aes(x=PCP26_C, y = ANEDUCA), color = "black", size=4, shape=17)+
+  labs(title = "Edad vs Nota final")+
   theme_minimal()
-
-ggsave("grafica.png", plot = g, width = 8, height = 6)
 
 g
 
